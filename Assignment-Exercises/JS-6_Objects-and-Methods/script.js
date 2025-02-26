774514d3f916618be00bea306bce3facabc989d6
Ceianreigh Keann M. Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2025-02-26 20:36:54 +0800

    Add function to describe car object

--- Assignment-Exercises/JS-6_Objects-and-Methods/script.js
+++ Assignment-Exercises/JS-6_Objects-and-Methods/script.js
@@ -6,5 +6,8 @@ const car = {
 };
 
 // Step 2: Create a function to describe the car
+function getDescription(car) {
+  return `This car is a ${car.year} ${car.make} ${car.model}`;
+}
 
 // Step 3: Call the function and display the result
