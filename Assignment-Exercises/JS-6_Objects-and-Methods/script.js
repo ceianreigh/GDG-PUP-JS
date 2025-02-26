98c0961ee9ba8402aa49effe5fa8123e158155f5
Ceianreigh Keann M. Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2025-02-26 20:35:13 +0800

    Add car object with make, model, and year properties

--- Assignment-Exercises/JS-6_Objects-and-Methods/script.js
+++ Assignment-Exercises/JS-6_Objects-and-Methods/script.js
@@ -1,8 +1,10 @@
 // Step 1: Create a car object
-
+const car = {
+  make: "Toyota",
+  model: "Corolla",
+  year: 2021,
+};
 
 // Step 2: Create a function to describe the car
 
-
 // Step 3: Call the function and display the result
-
