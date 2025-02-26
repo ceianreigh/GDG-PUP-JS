1b084610739da9bf8bd17121b646f97a66c3fc15
Ceianreigh Keann M. Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2025-02-26 20:38:31 +0800

    Fix punctuation in car description function

--- Assignment-Exercises/JS-6_Objects-and-Methods/script.js
+++ Assignment-Exercises/JS-6_Objects-and-Methods/script.js
@@ -7,7 +7,7 @@ const car = {
 
 // Step 2: Create a function to describe the car
 function getDescription(car) {
-  return `This car is a ${car.year} ${car.make} ${car.model}`;
+  return `This car is a ${car.year} ${car.make} ${car.model}.`;
 }
 
 // Step 3: Call the function and display the result
