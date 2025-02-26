2a9d034cf1a42c689894f41ea735d65803cbfb00
Ceianreigh Keann M. Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2025-02-26 14:22:15 +0800

    Add age category assignment in checkAge function

--- Assignment-Exercises/JS-3_Conditional-Statements/script.js
+++ Assignment-Exercises/JS-3_Conditional-Statements/script.js
@@ -4,4 +4,21 @@ function checkAge() {
 
   // Convert the input to a number
   age = Number(age);
+
+  // Initialize category variable
+  let category;
+
+  // Check the age and assign the category
+  if (age >= 0 && age <= 12) {
+    category = "Child";
+  } else if (age >= 13 && age <= 19) {
+    category = "Teenager";
+  } else if (age >= 20) {
+    category = "Adult";
+  }
+
+  // Check if the input is not a number
+  else {
+    category = "Invalid age. Please enter a valid number.";
+  }
 }
