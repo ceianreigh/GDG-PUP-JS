e8a404982a151f0e327996820fe6596dd730cc5a
Ceianreigh Keann M. Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2025-02-28 13:27:21 +0800

    Add event listener to initialize counter buttons on DOMContentLoaded

--- Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
+++ Assignment-Exercises/JS-8_DOM-Manipulation/JS8.js
@@ -1,1 +1,7 @@
 // your code starts here
+document.addEventListener("DOMContentLoaded", function () {
+  // select countertext, add button, and subtract button
+  const counterText = document.getElementById("counter-text");
+  const addButton = document.getElementById("button-add");
+  const subtractButton = document.getElementById("button-subtract");
+});
