735fa0b3ba32b4d203cdb01d615e7f8fbb631890
Ceianreigh Keann M. Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2025-02-28 13:13:20 +0800

    Add event listener to change background color on button click

--- Assignment-Exercises/JS-7_Event-Handling/script.js
+++ Assignment-Exercises/JS-7_Event-Handling/script.js
@@ -2,4 +2,7 @@
 const button = document.getElementById("colorButton");
 
 // add an event listener to the button
+button.addEventListener("click", function () {
+  document.body.style.backgroundColor = getRandomColor();
+});
 // when the button is clicked, change the background color of the body to a random color
