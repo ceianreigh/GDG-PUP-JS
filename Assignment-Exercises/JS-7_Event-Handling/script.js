3cf85ba0c90e343eaa75d84fa4df6be5bf432081
Ceianreigh Keann M. Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2025-02-28 13:12:22 +0800

    Add button selection for background color change event

--- Assignment-Exercises/JS-7_Event-Handling/script.js
+++ Assignment-Exercises/JS-7_Event-Handling/script.js
@@ -1,3 +1,5 @@
 // select the button using its id
+const button = document.getElementById("colorButton");
+
 // add an event listener to the button
 // when the button is clicked, change the background color of the body to a random color
